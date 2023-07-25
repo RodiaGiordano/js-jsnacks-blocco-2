9587d408a9512e41a49f26632434484671a72b90
RodiaGiordano <rodiagiordano93@gmail.com>
2023-07-25 15:37:44 +0200

    first snack complete

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const inputUser = parseInt(prompt("inserisci un numero"));
+
+
+
+if(isNaN(inputUser) || !(inputUser % 2 == 0)){
+    let numberOdd = inputUser + 1
+
+    alert(numberOdd)
+}
+
+
+else{1
+    alert(inputUser)
+}
+
+
+console.log(inputUser);
+console.log(typeof inputUser);
+
