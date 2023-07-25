ffc708de6c48ee98eff0c77ce4308f132bcf6e64
RodiaGiordano <rodiagiordano93@gmail.com>
2023-07-25 17:36:03 +0200

    third snack

---
+++ js/script_3.js
@@ -0,0 +1,33 @@
+                
+const listNumber = [10, 11, 20, 30, 40, 50, 60, 70, 80, 90];
+                  
+// let max = listNumber.length;
+
+
+let somma = "";
+
+
+for(i= 0; i < listNumber.length; i++ ){
+
+    let even = listNumber[i] ;
+
+    console.log(typeof even)
+
+    if(i % 2 !== 0){
+
+        
+        
+        somma += even
+        
+        
+
+        
+    }
+    
+
+}
+
+
+    console.log(somma)
+
+    
