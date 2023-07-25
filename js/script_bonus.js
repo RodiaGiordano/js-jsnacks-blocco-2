585204453a5a86cca52d6815ee07f4debbf8c9f4
RodiaGiordano <rodiagiordano93@gmail.com>
2023-07-25 23:02:16 +0200

    feat: fix odd result

--- js/script_bonus.js
+++ js/script_bonus.js
@@ -1,33 +1,73 @@
-const chestOne = ['apple', 'apple', 'apple', 'apple', 'apple', 'apple', 'apple', 'apple'];
+const chestTwo = ['apple', 'apple', 'apple', 'apple', 'apple', 'apple', 'apple', 'apple'];
 
-const chestTwo = [];
+const chestOne = ['apple'];
 
-let itermRemove = "";
-let itemAdd = "";
+let media = (chestOne.length + chestTwo.length) / 2;
 
-const media = (chestOne.length + chestTwo.length) / 2;
+if(media % 2 !== 0 && chestOne.length > chestTwo.length){
+    itemEat = chestOne.shift();
+    
+}
 
-console.log('media :' + media);
-
-for(i = 0; i <= media; i++){
-
-    if( chestOne.length > media ){
-        
-        itemRemove = chestOne.shift();
-
-        itemAdd = chestTwo.push(itemRemove);
-    }
-
-    else if( chestTwo.length > media ){
-        
-        itemRemove = chestTwo.shift();
-
-        itemAdd = chestOne.push(itemRemove);
-    }
-
-
+else if(media % 2 !== 0 && chestOne.length < chestTwo.length){
+    itemEat = chestTwo.shift();
+    
 
 }
 
-console.log('cesto 1: ' + chestOne)
-console.log('cesto 2: ' + chestTwo)
+media = (chestOne.length + chestTwo.length) / 2;
+
+
+ for(i = 0; i < media; i++){
+
+     if(media % 2 !== 0 && chestOne.length > chestTwo.length && chestOne > media){
+                        
+    
+
+     media = (chestOne.length + chestTwo.length) / 2
+
+     itemRemove = chestOne.shift();
+
+     itemAdd = chestTwo.push(itemRemove)
+
+    
+     }   
+
+     else if(media % 2 !== 0 && chestOne.length < chestTwo.length && chestOne < media){
+         chestTwo.shift();
+
+         itemRemove = chestTwo.shift();
+            
+         itemAdd = chestOne.push(itemRemove);
+
+         media = (chestOne.length + chestTwo.length) / 2
+
+     }
+
+     else if(chestOne.length > media ){
+        
+         itemRemove = chestOne.shift();
+
+         itemAdd = chestTwo.push(itemRemove);
+
+         media = (chestOne.length + chestTwo.length) / 2
+     }
+
+     else if( chestTwo.length > media ){
+        
+         itemRemove = chestTwo.shift();
+
+         itemAdd = chestOne.push(itemRemove);
+
+         media = (chestOne.length + chestTwo.length) / 2
+     }
+          
+
+    
+ }
+
+    
+     console.log('media :' + media);
+     console.log('cesto 1: ' + chestOne)
+     console.log('cesto 2: ' + chestTwo)
+    
